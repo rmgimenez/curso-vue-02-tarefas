0c8f91e84a7464be017857f211070ddcdcc8398e
Ricardo Gimenez <rmgimenez@gmail.com>
2022-05-03 22:51:42 -0300

    mudanças no css

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="task-grid">
-      <template>
+      <template v-if="tasks.length">
           <Task v-for="task in tasks" :key="task.name" :task="task">{{ task.name }}</Task>
       </template>
+      <p class="no-task" v-else>Sua vida está em dia :)</p>
   </div>
 </template>
 
@@ -18,30 +19,18 @@ export default {
 </script>
   
 <style>
-  .task {
-    box-sizing: border-box;
-    width: 350px;
-    height: 150px;
-    padding: 10px;
-    border-radius: 8px;
-    font-size: 2rem;
-    font-weight: 300;
-    cursor: pointer;
-    user-select: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.task-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
 
-  .pending {
-    border-left: 12px solid #b73229;
-    background-color: #f44336;
-  }
+.task-grid .task {
+  margin: 10px;
+}
 
-  .done {
-    color: #ddd;
-    border-left: 12px solid #0a8f08;
-    background-color: #4caf50;
-    text-decoration: line-through;
-  }
+.no-task {
+  color: #aaa;
+  font-size: 1.7rem;
+}
 </style>
